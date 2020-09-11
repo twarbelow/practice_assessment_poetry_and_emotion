9161a4be69200fd3651369f5566e77ac3e189d42
Dione Wilson <dionew1@gmail.com>
2020-09-11 13:30:48 -0600

    Add comments

--- app/poros/poem.rb
+++ app/poros/poem.rb
@@ -1,3 +1,6 @@
+# Good job joining the lines together into a single string.
+# In the initialize method I would include an attribute of tone and set it to be empty, just to make it a little clearer that it is an attribute of a poem
+
 class Poem
   attr_accessor :title, :author, :tone
   def initialize(info)
--- app/services/tone_service.rb
+++ app/services/tone_service.rb
@@ -1,3 +1,6 @@
+# Great job making the url and api key dynamic
+# Rather than including the params as part of the path break it into a block for better readability
+
 class ToneService
   def get_tone(text)
     response = conn.get("/v3/tone?version=2017-09-21&text=#{text}")
